0dd2123809a5eb1e8484dec379c1c3a18a27e667
Prem877 <premsukh7691@gmail.com>
2025-03-23 11:10:05 +0530

    now some changes in eslint for no-var

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,16 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-// const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-// if (process.env.NODE_ENV !== "production") {
-//   global.prisma = prisma;
-// }
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
-// export default prisma;
+export default prisma;
 //******************************************************************* */
 
 // import { PrismaClient } from "@prisma/client";
@@ -31,23 +31,18 @@
 
 // export default prisma;
 
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
 
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-    }
-  }
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+// declare global {
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+// const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
-export default prisma;
+// export default prisma;

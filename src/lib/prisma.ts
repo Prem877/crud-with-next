8874f9b9d97cc0a8e0cfa9f87352a574c5459298
Prem877 <premsukh7691@gmail.com>
2025-03-23 10:53:29 +0530

    now database direct url is added

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -33,6 +33,16 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export default prisma;

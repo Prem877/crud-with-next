b11e576d917f3af4bc0a8ebbdc326d302b3f0983
Prem877 <premsukh7691@gmail.com>
2025-03-27 12:17:31 +0530

    now type error solved for delete user function

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,13 +1,38 @@
+// import { NextResponse } from "next/server";
+// import prisma from "@/lib/prisma";
+
+// export async function DELETE(
+//   req: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   try {
+//     await prisma.user.delete({
+//       where: { id: params.id },
+//     });
+
+//     return NextResponse.json(
+//       { message: "User deleted successfully" },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error("Error deleting user:", error);
+//     return NextResponse.json(
+//       { error: "Failed to delete user" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { id: any } }
-) {
+export async function DELETE(req: Request, context: any) {
+  const { params } = context;
+  const id = params.id as string; // Assert id as string
+
   try {
     await prisma.user.delete({
-      where: { id: params.id },
+      where: { id },
     });
 
     return NextResponse.json(
